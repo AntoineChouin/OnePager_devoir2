c3216a587d2d8fa4a57d04ab05271a90f5335b99
Antoine Chouinard <antoine.brochu@me.com>
2017-12-22 12:42:04 -0500

    Finalement
    On ajoute les derniers petit trucs

--- js/main.js
+++ js/main.js
@@ -6,47 +6,65 @@ var Microbiere = new Vue({
     el: '#app',
     data: {
         bieres: [{
-                nom: 'Imposteur',
+                nom: "Imposteur",
+                class: "schwarzbier",
                 histoire: " À l'honneur des Schwarzbier, de ses bieres blondes qui se sont teintues en noir",
-                style: "Schwarzbier",
+                style: "schwarzbier",
                 abv: "4%",
-                ibu: "25",
-                contenant: "canette",
-                Veillissement: "Non",
-                saveur: "Vanille, chocolant, grillé"
+                ibu: "20",
+                format: "canette",
+                maturite: "Non",
+                saveur: "Vanille, chocolant, grillé",
+                img: "../../image/Panther_Paw.svg"
             },
             {
                 nom: 'Ourson de Berlin',
                 histoire: "À Berlin, on fait comme les Berlinois. On encourage l'Ourson",
+                class: "berliner",
                 style: "Berliner Weisse",
-                abv: "3,6%",
-                format: "Canette",
-                saveur: "suret léger, Vanille, Argrume"
+                abv: "2.5%",
+                ibu: "25",
+                format: "canette",
+                maturite: "Non",
+                saveur: "suret léger, Vanille, Argrume",
+                 img: "../../image/Panther_Paw.svg"
             },
 
             {
                 nom: 'Halt',
                 histoire: "Une rousse bien sexy en Allemangne, ca fait fureur!",
                 style: "Altbier",
+                class: "altbier",
                 abv: "6.5%",
+                ibu: "25",
                 format: "canette",
-                saveur: "orange"
+                maturite: "Non",
+                saveur: "orange",
+                 img: "../../image/Panther_Paw.svg"
             },
             {
                 nom: 'Dentelle',
-                histoire: "Biere obscure, n'ayant pas de dentelle",
+                histoire: "De la dentelle, ce n'est pas pour elle",
                 style: "Dunkel",
+                class: "dunkel",
                 abv: "5.5%",
+                 ibu: "25",
                 format: "canette",
-                saveur: "orange"
+                 maturite: "Non",
+                saveur: "orange",
+                 img: "../../image/Panther_Paw.svg"
             },
             {
-                nom: 'Oiseau',
-                histoire: "“Help me, Obi-Wan Kenobi. You’re my only hope.” — Leia Organa",
+                nom: 'Raison',
+                histoire: "Pour nourrir ",
                 style: "Weizenbock",
+                class: "weizenbock",
                 abv: "10.5%",
-                format: "canette",
-                saveur: "orange"
+                ibu: "65",
+                 maturite: "oui, 5 ans",
+                format: "bouteille",
+                saveur: "cassonade, alcool, sucre d'orge",
+                 img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Panther_Paw.svg/628px-Panther_Paw.svg.png"
             }
         ]
     }
